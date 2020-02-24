cc466785163d0873a429bad168752aca8ad96898
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-02-25 01:10:15 +0800

    Add leaflet

---
+++ FlightEvents.Web/ClientApp/src/components/Home.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as L from 'leaflet';
+
+export class Home extends React.Component {
+    static displayName = Home.name;
+
+    componentDidMount() {
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(mymap);
+    }
+
+    render() {
+        return (
+            <div id="mapid" style={{ height: 360 }}></div>
+        );
+    }
+}
