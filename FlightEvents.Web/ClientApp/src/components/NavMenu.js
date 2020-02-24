3163ccd3d874d389a0136c6f6a48f00a27aff644
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-02-25 01:12:41 +0800

    Remove sample pages

--- FlightEvents.Web/ClientApp/src/components/NavMenu.js
+++ FlightEvents.Web/ClientApp/src/components/NavMenu.js
@@ -26,18 +26,12 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">FlightEvent.Web</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Flight Events</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
