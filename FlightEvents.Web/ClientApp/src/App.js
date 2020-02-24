c8c171d8c9bc9068c9e5c8fcd292991f31fd3dcc
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-02-25 02:34:11 +0800

    fixup! Remove sample pages

--- FlightEvents.Web/ClientApp/src/App.js
+++ FlightEvents.Web/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -14,8 +12,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
       </Layout>
     );
   }
