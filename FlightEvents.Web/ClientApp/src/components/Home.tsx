9e10c36ea3f4ced556000924a3a4028ca15cf23b
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-02-25 21:05:15 +0800

    Allow changing map layer

--- FlightEvents.Web/ClientApp/src/components/Home.tsx
+++ FlightEvents.Web/ClientApp/src/components/Home.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import * as L from 'leaflet';
 import 'leaflet-rotatedmarker';
 import * as signalr from '@microsoft/signalr';
 import { AircraftStatus } from '../Models';
 import AircraftList from './AircraftList';
+
+
+enum MapTileType {
+    OpenStreetMap,
+    OpenTopoMap,
+    EsriWorld
+}
 
 interface State {
     aircrafts: { [connectionId: string]: AircraftStatus };
@@ -13,6 +21,7 @@ export class Home extends React.Component<any, State> {
     static displayName = Home.name;
 
     mymap: L.Map;
+    baseLayerGroup: L.LayerGroup;
     markers: { [connectionId: string]: L.Marker<any> } = {}
 
     constructor(props: any) {
@@ -23,18 +32,16 @@ export class Home extends React.Component<any, State> {
         }
 
         this.handleAircraftClick = this.handleAircraftClick.bind(this);
+        this.handleOpenStreetMap = this.handleOpenStreetMap.bind(this);
+        this.handleOpenTopoMap = this.handleOpenTopoMap.bind(this);
+        this.handleEsriWorld = this.handleEsriWorld.bind(this);
     }
 
     componentDidMount() {
         this.mymap = L.map('mapid').setView([51.505, -0.09], 13);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'your.mapbox.access.token'
-        }).addTo(this.mymap);
+
+        this.baseLayerGroup = L.layerGroup().addTo(this.mymap);
+        this.setTileLayer(MapTileType.OpenStreetMap);
 
         let hub = new signalr.HubConnectionBuilder()
             .withUrl('/FlightEventHub')
@@ -82,6 +89,33 @@ export class Home extends React.Component<any, State> {
         hub.start();
     }
 
+    private setTileLayer(type: MapTileType) {
+        this.baseLayerGroup.clearLayers();
+        switch (type) {
+            case MapTileType.OpenStreetMap:
+                this.baseLayerGroup.addLayer(L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'mapbox/streets-v11',
+                    tileSize: 512,
+                    zoomOffset: -1,
+                    accessToken: 'your.mapbox.access.token'
+                }));
+                break;
+            case MapTileType.OpenTopoMap:
+                this.baseLayerGroup.addLayer(L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 17,
+                    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+                }));
+                break;
+            case MapTileType.EsriWorld:
+                this.baseLayerGroup.addLayer(L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+                    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                }));
+                break;
+        }
+    }
+
     private handleAircraftClick(connectionId: string, aircraftStatus: AircraftStatus) {
         if (this.mymap) {
             let latlng: L.LatLngExpression = [aircraftStatus.latitude, aircraftStatus.longitude];
@@ -89,10 +123,38 @@ export class Home extends React.Component<any, State> {
         }
     }
 
+    private handleOpenStreetMap() {
+        this.setTileLayer(MapTileType.OpenStreetMap);
+    }
+
+    private handleOpenTopoMap() {
+        this.setTileLayer(MapTileType.OpenTopoMap);
+    }
+
+    private handleEsriWorld() {
+        this.setTileLayer(MapTileType.EsriWorld);
+    }
+
     render() {
         return <>
             <div id="mapid" style={{ height: '100%' }}></div>
+            <LayerWrapper>
+                <button onClick={this.handleOpenStreetMap}>OpenStreetMap</button>
+                <button onClick={this.handleOpenTopoMap}>OpenTopoMap</button>
+                <button onClick={this.handleEsriWorld}>Esri</button>
+            </LayerWrapper>
             <AircraftList aircrafts={this.state.aircrafts} onAircraftClick={this.handleAircraftClick} />
         </>;
     }
 }
+
+const LayerWrapper = styled.div`
+position: absolute;
+top: 80px;
+left: 5px;
+z-index: 10000;
+
+button {
+    display: block;
+}
+`;
