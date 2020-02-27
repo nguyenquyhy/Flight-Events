d34362484ddcc4e282978a82dd50588cf6a6de10
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-02-27 15:55:00 +0800

    Change aircraft list to show columns

--- FlightEvents.Web/ClientApp/src/components/AircraftList.tsx
+++ FlightEvents.Web/ClientApp/src/components/AircraftList.tsx
@@ -35,55 +35,71 @@ export default class AircraftList extends React.Component<Props> {
     public render() {
         const connectionIds = Object.keys(this.props.aircrafts);
         const list = connectionIds.length === 0 ?
-            <NoneText>None</NoneText> :
+            <tr><td colSpan={3}><NoneText>None</NoneText></td></tr> :
             connectionIds.map(connectionId => (
                 <ListItem key={connectionId}>
-                    <button className="btn btn-link" onClick={() => this.props.onAircraftClick(connectionId, this.props.aircrafts[connectionId])}>
-                        {this.props.aircrafts[connectionId].callsign || connectionId.substring(5)}
-                    </button>
-                    <CheckWrapper><input type="checkbox" checked={this.props.myConnectionId === connectionId} onChange={() => this.handleMeChanged(connectionId)} /> Me</CheckWrapper>
-                    <CheckWrapper><input type="checkbox" checked={this.props.followingConnectionId === connectionId} onChange={() => this.handleFollowChanged(connectionId)} /> Follow</CheckWrapper>
+                    <td>
+                        <button className="btn btn-link" onClick={() => this.props.onAircraftClick(connectionId, this.props.aircrafts[connectionId])}>
+                            {this.props.aircrafts[connectionId].callsign || connectionId.substring(5)}
+                        </button>
+                    </td>
+                    <td><Checkbox type="checkbox" checked={this.props.myConnectionId === connectionId} onChange={() => this.handleMeChanged(connectionId)} /></td>
+                    <td><Checkbox type="checkbox" checked={this.props.followingConnectionId === connectionId} onChange={() => this.handleFollowChanged(connectionId)} /></td>
                 </ListItem>
             ));
 
         return <Wrapper>
-            <Title>Aircrafts</Title>
-            <List>{list}</List>
+            <List>
+                <thead>
+                    <tr>
+                        <th><Title>Aircrafts</Title></th>
+                        <th><div style={{ marginRight: 3, marginLeft: 3 }}>Me</div></th>
+                        <th>Follow</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list}
+                </tbody>
+            </List>
         </Wrapper>
     }
 }
 
 const Title = styled.div`
-margin: 10px 10px 0px 10px;
+margin-left: 8px;
 font-weight: bold;
 font-style: italic;
 text-align: center;
 `
 
 const NoneText = styled.div`
-margin: 0 10px;
+margin: 0 8px;
 `
 
 const Wrapper = styled(Panel)`
 position: absolute;
 top: 10px;
 right: 10px;
-z-index: 10000;;
+z-index: 1000;
 `
 
-const List = styled.ul`
+const List = styled.table`
+margin-top: 10px;
 list-style: none;
 padding: 0;
 `
 
-const ListItem = styled.li`
+const ListItem = styled.tr`
 button {
 display: block;
 font-weight: bold;
 width: 100%;
 }
+td {
+text-align: center;
+}
 `
 
-const CheckWrapper = styled.label`
-margin: 0 10px;
+const Checkbox = styled.input`
+text-align: center;
 `
