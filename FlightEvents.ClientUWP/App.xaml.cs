a95f002cd5466bfe2dba6d1ee4040d1a0c9db4ff
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-04-13 15:01:29 +0800

    Initial project for GameBar widget

---
+++ FlightEvents.ClientUWP/App.xaml.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Gaming.XboxGameBar;
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace FlightEvents.ClientUWP
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private XboxGameBarWidget widget;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            if (args.Kind == ActivationKind.Protocol && args is IProtocolActivatedEventArgs protocolArgs)
+            {
+                var scheme = protocolArgs.Uri.Scheme;
+                if (scheme == "ms-gamebarwidget" && args is XboxGameBarWidgetActivatedEventArgs widgetArgs)
+                {
+                    if (widgetArgs.IsLaunchActivation)
+                    {
+                        // Game Bar Widget activation work here
+                        var rootFrame = new Frame();
+                        rootFrame.NavigationFailed += RootFrame_NavigationFailed;
+                        Window.Current.Content = rootFrame;
+
+                        widget = new XboxGameBarWidget(widgetArgs, Window.Current.CoreWindow, rootFrame);
+
+                        Window.Current.Closed += Current_Closed;
+
+                        rootFrame.Navigate(typeof(MainPage));
+                        Window.Current.Activate();
+                    }
+                    else
+                    {
+                        // Repeat activation for this widget. Ignore, or respond to URI payload
+                    }
+                }
+            }
+        }
+
+        private void Current_Closed(object sender, Windows.UI.Core.CoreWindowEventArgs e)
+        {
+            // Release widget object
+            widget = null;
+            // Unregister from window closed event
+            Window.Current.Closed -= Current_Closed;
+        }
+
+        private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
