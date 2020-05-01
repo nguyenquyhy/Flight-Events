8e7d7ba68b663294dab742fb705ef9fc6785f0db
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-05-02 05:57:47 +0800

    GpsHelper should not be affected by computer locale now

--- FlightEvents.Common/GpsHelper.cs
+++ FlightEvents.Common/GpsHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace FlightEvents
 {
     public class GpsHelper
     {
-        private static Regex re = new Regex(@"([N|S])([0-9]*[\.[0-9]+]*)[*|°]\s([0-9]*[\.[0-9]+]*)'\s([0-9]*[\.[0-9]+]*)"",([W|E])([0-9]*[\.[0-9]+]*)[*|°]\s([0-9]*[\.[0-9]+]*)'\s([0-9]*[\.[0-9]+]*)""");
+        private static readonly Regex re = new Regex(@"([N|S])([0-9]*[\.[0-9]+]*)[*|°]\s([0-9]*[\.[0-9]+]*)'\s([0-9]*[\.[0-9]+]*)"",([W|E])([0-9]*[\.[0-9]+]*)[*|°]\s([0-9]*[\.[0-9]+]*)'\s([0-9]*[\.[0-9]+]*)""");
 
         public static (double latitude, double longitude) ConvertString(string data)
         {
@@ -17,12 +18,12 @@ namespace FlightEvents
 
             if (m.Success)
             {
-                lt = Convert.ToDouble(m.Groups[2].Value) + (Convert.ToDouble(m.Groups[3].Value) / 60.0) + (Convert.ToDouble(m.Groups[4].Value) / 3600.0);
+                lt = Convert.ToDouble(m.Groups[2].Value, CultureInfo.InvariantCulture) + (Convert.ToDouble(m.Groups[3].Value, CultureInfo.InvariantCulture) / 60.0) + (Convert.ToDouble(m.Groups[4].Value, CultureInfo.InvariantCulture) / 3600.0);
                 if (m.Groups[1].Value.Contains("S"))
                 {
                     lt *= -1.0;
                 }
-                ln = Convert.ToDouble(m.Groups[6].Value) + (Convert.ToDouble(m.Groups[7].Value) / 60.0) + (Convert.ToDouble(m.Groups[8].Value) / 3600.0);
+                ln = Convert.ToDouble(m.Groups[6].Value, CultureInfo.InvariantCulture) + (Convert.ToDouble(m.Groups[7].Value, CultureInfo.InvariantCulture) / 60.0) + (Convert.ToDouble(m.Groups[8].Value, CultureInfo.InvariantCulture) / 3600.0);
                 if (m.Groups[5].Value.Contains("W"))
                 {
                     ln *= -1.0;
