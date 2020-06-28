914d69a18e832adc4d5ae384f77333255f25758d
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-06-28 20:29:07 +0800

    Improve status indicator

---
+++ FlightEvents.Client/Converters/NullToBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FlightEvents.Client.Converters
+{
+    public class NullToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case null: return new SolidColorBrush(Colors.Gray); 
+                default: return new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlightEvents.Client/Converters/StatesToBrushConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FlightEvents.Client.Converters
+{
+    public class StatesToBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length >= 2 && values[0] is ConnectionState simconnectState && values[1] is bool trackingState)
+            {
+                switch (simconnectState)
+                {
+                    case ConnectionState.Failed: return new SolidColorBrush(Colors.Red);
+                    case ConnectionState.Idle: return new SolidColorBrush(Colors.Gray);
+                    case ConnectionState.Connecting: return new SolidColorBrush(Colors.Gray);
+                    case ConnectionState.Connected:
+                        if (trackingState) return new SolidColorBrush(Colors.Green);
+                        else return new SolidColorBrush(Colors.Gray);
+                }
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
