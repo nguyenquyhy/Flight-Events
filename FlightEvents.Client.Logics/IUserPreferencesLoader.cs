2df194a9dbcdb282a01ecf7420694f5f07067a9f
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-07-03 02:30:00 +0800

    WIP: MSIX

---
+++ FlightEvents.Client.Logics/IUserPreferencesLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FlightEvents.Client.Logics
+{
+    public interface IUserPreferencesLoader
+    {
+        Task<UserPreferences> LoadAsync();
+        Task<UserPreferences> UpdateAsync(Action<UserPreferences> updateAction);
+    }
+}
