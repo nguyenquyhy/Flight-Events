d62464703deab14559145ecd2ceaf26dd79a7c22
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-12-06 16:07:56 -0500

    ATIS: Update Serilog host setup

--- FlightEvents.DiscordBot.AtisBot/Program.cs
+++ FlightEvents.DiscordBot.AtisBot/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -10,33 +10,41 @@ namespace FlightEvents.DiscordBot.AtisBot
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(assemblyDirectory, "flightevents-bot-atis.log"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+                return 1;
+            }
+            finally
+            {
+                Log.Information("Stopping host");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseWindowsService()
+                .UseSerilog()
                 .ConfigureAppConfiguration(configBuilder =>
                 {
                     configBuilder.AddJsonFile("appsettings.Release.json", optional: true);
-                })
-                .ConfigureLogging(logging =>
-                {
-                    var assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-                    Log.Logger = new LoggerConfiguration()
-                        .MinimumLevel.Debug()
-                        .WriteTo.Debug()
-                        .WriteTo.Console()
-                        .WriteTo.File(Path.Combine(assemblyDirectory, "flightevents-bot-atis.log"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
-                        .CreateLogger();
-
-                    logging
-                        .ClearProviders()
-                        .AddSerilog()
-                        .AddEventSourceLogger();
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
