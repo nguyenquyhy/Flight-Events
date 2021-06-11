904deea9db7fcec676978141ad96eab697f3dd2c
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2021-06-11 14:09:15 -0400

    Add Serilog for web

--- FlightEvents.Web/Program.cs
+++ FlightEvents.Web/Program.cs
@@ -1,6 +1,8 @@
 using MessagePack;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace FlightEvents.Web
 {
@@ -8,13 +10,35 @@ namespace FlightEvents.Web
     {
         public static void Main(string[] args)
         {
-            MessagePackSecurity.Active = MessagePackSecurity.UntrustedData;
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .CreateBootstrapLogger();
 
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting up");
+                MessagePackSecurity.Active = MessagePackSecurity.UntrustedData;
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- FlightEvents.Web/Startup.cs
+++ FlightEvents.Web/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
@@ -172,6 +173,8 @@ namespace FlightEvents.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
