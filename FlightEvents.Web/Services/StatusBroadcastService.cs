fb2bcd507a2a9607d2f2d51314a7969b2e8ab2f7
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2021-08-01 19:34:53 -0400

    Add logging for error in StatusBroadcastService

--- FlightEvents.Web/Services/StatusBroadcastService.cs
+++ FlightEvents.Web/Services/StatusBroadcastService.cs
@@ -1,6 +1,7 @@
 using FlightEvents.Web.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Linq;
@@ -20,38 +21,48 @@ namespace FlightEvents.Web
 
         private readonly IHubContext<FlightEventHub, IFlightEventHub> hubContext;
         private readonly IOptionsMonitor<BroadcastOptions> optionsAccessor;
+        private readonly ILogger<StatusBroadcastService> logger;
 
-        public StatusBroadcastService(IHubContext<FlightEventHub, IFlightEventHub> hubContext, IOptionsMonitor<BroadcastOptions> optionsAccessor)
+        public StatusBroadcastService(IHubContext<FlightEventHub, IFlightEventHub> hubContext, IOptionsMonitor<BroadcastOptions> optionsAccessor, ILogger<StatusBroadcastService> logger)
         {
             this.hubContext = hubContext;
             this.optionsAccessor = optionsAccessor;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (true)
             {
-                await Task.Delay(optionsAccessor.CurrentValue.MapDelayMilliseconds);
-                stoppingToken.ThrowIfCancellationRequested();
-
-                var statuses = FlightEventHub.ConnectionIdToAircraftStatuses.ToList();
-                foreach (var pair in statuses)
+                try
                 {
-                    if (DateTimeOffset.Now - pair.Value.updateTime < timeout
-                        && FlightEventHub.ConnectionIdToClientIds.TryGetValue(pair.Key, out var clientId))
-                    {
-                        await hubContext.Clients.Groups("Map", "ClientMap").UpdateAircraft(clientId, pair.Value.status);
+                    await Task.Delay(optionsAccessor.CurrentValue.MapDelayMilliseconds);
+                    stoppingToken.ThrowIfCancellationRequested();
 
-                        await hubContext.Clients.Groups("Admin").UpdateAircraft(clientId, new AircraftStatus(pair.Value.status)
-                        {
-                            ClientVersion = FlightEventHub.ConnetionIdToClientInfos.ContainsKey(pair.Key) ? FlightEventHub.ConnetionIdToClientInfos[pair.Key].ClientVersion : "?"
-                        });
-                    }
-                    else
+                    var statuses = FlightEventHub.ConnectionIdToAircraftStatuses.ToList();
+                    foreach (var pair in statuses)
                     {
-                        // Client is probably removed, it is better to remove cache too
-                        FlightEventHub.RemoveCacheOnConnectionId(pair.Key);
+                        if (DateTimeOffset.Now - pair.Value.updateTime < timeout
+                            && FlightEventHub.ConnectionIdToClientIds.TryGetValue(pair.Key, out var clientId))
+                        {
+                            await hubContext.Clients.Groups("Map", "ClientMap").UpdateAircraft(clientId, pair.Value.status);
+
+                            await hubContext.Clients.Groups("Admin").UpdateAircraft(clientId, new AircraftStatus(pair.Value.status)
+                            {
+                                ClientVersion = FlightEventHub.ConnetionIdToClientInfos.ContainsKey(pair.Key) ? FlightEventHub.ConnetionIdToClientInfos[pair.Key].ClientVersion : "?"
+                            });
+                        }
+                        else
+                        {
+                            // Client is probably removed, it is better to remove cache too
+                            FlightEventHub.RemoveCacheOnConnectionId(pair.Key);
+                        }
                     }
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Cannot execute status broadcast!");
+                    await Task.Delay(10000);
                 }
             }
         }
