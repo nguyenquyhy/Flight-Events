064acf62aab556e7f5c42d64399b4f4decafd9b5
Nguyễn Quý Hy <nguyenquyhy@live.com.sg>
2023-07-04 11:55:49 -0400

    Upgrade react-scripts to 4.0.3 and remove craco (#66)
    [Notes]:
    Since cra now use InjectManifest instead of GenerateSw, craco is no longer needed for workbox customization.
    The project is not adapted to align to cra pwa template.

--- FlightEvents.Web/ClientApp/src/index.js
+++ FlightEvents.Web/ClientApp/src/index.js
@@ -1,10 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -31,5 +30,16 @@ ReactDOM.render(
     </ApolloProvider>,
     rootElement);
 
-registerServiceWorker();
+serviceWorkerRegistration.register({
+    onUpdate: (registration) => {
+        var element = document.getElementById("divUpdateMsg");
+        element.innerHTML =
+            "An updated version of this website is available. Please reload the page.";
+        element.style.display = "block";
 
+        if (registration && registration.waiting) {
+            // Skip waiting to remove the requirement to restart browser
+            registration.waiting.postMessage({ type: "SKIP_WAITING" });
+        }
+    }
+})
